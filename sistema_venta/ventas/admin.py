d36a82f13f89e40b2c8ab9474e157641e3eaa13f
ANTONIOS-MAJ-HE <antonio.santiago@tecsup.edu.pe>
2023-06-28 23:31:36 -0500

    visualizacion en admin

--- sistema_venta/ventas/admin.py
+++ sistema_venta/ventas/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-
+from .models import Producto, Tipopago, Categoria, Cliente, Empleado, Pago, Venta, Detalleventa
 # Register your models here.
 
+admin.site.register(Producto)
+admin.site.register(Tipopago)
+admin.site.register(Categoria)
+admin.site.register(Cliente)
+admin.site.register(Empleado)
+admin.site.register(Pago)
+admin.site.register(Venta)
+admin.site.register(Detalleventa)
