9926fc613d0dfa3f5af3b6be0c6a65a33868d1e1
Kenziefor <myriam.mamani@tecsup.edu.pe>
2023-06-29 09:32:18 -0500

    venta commit

--- sistema_venta/ventas/urls.py
+++ sistema_venta/ventas/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'ventas'
 urlpatterns = [
 
     path('', views.homes, name='home'),
@@ -8,5 +10,8 @@ urlpatterns = [
     path('signout/', views.signout, name='signout'),
     path('signup/', views.signup, name='signup'),
     path('profile/', views.profile, name='profile'),
-    
+    path('dtventas/', views.listadoventa, name='dtventas'),
+    path('registrarventa/', views.registrarventa, name='registrarventa'),
+    path('dtventas/eliminarventa/<idVenta>', views.eliminarventa),
+    path('dtventas/editarventa/<int:id_venta>', views.editar_venta, name='editar_venta'),
 ]
--- sistema_venta/ventas/views.py
+++ sistema_venta/ventas/views.py
@@ -1,6 +1,10 @@
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Venta, Empleado, Cliente
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
+from datetime import datetime
 # Create your views here.
 
 def homes(request): 
@@ -52,3 +56,102 @@ def profile(request):
 def signout(request):
     logout(request)
     return redirect('/')
+
+
+# ListadoVenta
+def listadoventa(request):
+    venta_list = Venta.objects.all()
+    return render(request, "VentaListado.html", {"ventas": venta_list})
+
+
+def registrarventa(request):
+    if request.method == 'POST':
+        codigo = request.POST['txtcodigo']
+        fecha = request.POST['txtfecha']
+        subtotal = request.POST['txtsubtotal']
+        igv = request.POST['txtigv']
+        monto = request.POST['txtmonto']
+        id_cliente = request.POST['cliente']
+        id_empleado = request.POST['empleado']
+
+        cliente = Cliente.objects.get(pk=id_cliente)
+        empleado = Empleado.objects.get(pk=id_empleado)
+
+        venta = Venta(
+            idVenta=codigo,
+            fecha=fecha,
+            subtotal=subtotal,
+            igv=igv,
+            monto=monto,
+            idcliente=cliente,
+            idempleado=empleado
+        )
+        venta.save()
+
+        # Redirige a la página de listado de ventas
+        return redirect('ventas:dtventas')
+
+    clientes = Cliente.objects.all()
+    empleados = Empleado.objects.all()
+
+    return render(request, 'VentaRegistro.html', {'clientes': clientes, 'empleados': empleados})
+
+
+def eliminarventa(request, idVenta):
+
+    venta = Venta.objects.get(idVenta=idVenta)
+    venta.delete()
+    return redirect('/dtventas/')
+
+
+def editar_venta(request, id_venta):
+    venta = get_object_or_404(Venta, idVenta=id_venta)
+    empleados = Empleado.objects.all()
+    clientes = Cliente.objects.all()
+
+    if request.method == 'POST':
+        # Obtener los datos del formulario
+        id_cliente = request.POST.get('id_cliente')
+        id_empleado = request.POST.get('id_empleado')
+        fecha = request.POST.get('fecha')
+        subtotal = request.POST.get('subtotal')
+        igv = request.POST.get('igv')
+        monto = request.POST.get('monto')
+# Convertir la fecha al formato correcto
+        fecha = datetime.strptime(fecha, '%B %d, %Y').strftime('%Y-%m-%d')
+
+
+        # Obtener las instancias de Cliente y Empleado
+        cliente = get_object_or_404(Cliente, idCliente=id_cliente)
+        empleado = get_object_or_404(Empleado, idEmpleado=id_empleado)
+
+        # Actualizar los campos de la venta
+        venta.idcliente = cliente
+        venta.idempleado = empleado
+        venta.fecha = fecha
+        venta.subtotal = subtotal
+        venta.igv = igv
+        venta.monto = monto
+
+        # Guardar los cambios en la venta
+        venta.save()
+
+        # Redireccionar a una página de éxito o a la página de detalles de la venta
+        return redirect('/dtventas/', id_venta=venta.idVenta)
+
+    return render(request, 'VentaEditar.html', {'venta': venta, 'empleados': empleados, 'clientes': clientes})
+# BUSCAR-----------------
+
+
+def buscarespecialidad(request):
+    buscar_especialidad = request.POST.get("buscar")
+    especialidades = Especialidad.objects.all()
+
+    if buscar_especialidad:
+        especialidades = Especialidad.objects.filter(
+            Q(nombre__icontains=buscar_especialidad)
+        ).distinct()
+    datos = {
+        'especialidades': especialidades
+    }
+    return render(request, 'ListaEspecialidad.html', datos)
