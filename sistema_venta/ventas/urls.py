32e7c05a1d97464d1f6ef8b25ec9ee9f3230085e
ANTONIOS-MAJ-HE <antonio.santiago@tecsup.edu.pe>
2023-06-29 11:57:11 -0500

    editando de sharon productos

--- sistema_venta/ventas/urls.py
+++ sistema_venta/ventas/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('signout/', views.signout, name='signout'),
     path('signup/', views.signup, name='signup'),
     path('profile/', views.profile, name='profile'),
-<<<<<<< HEAD
 
     ##Producto-----------------------------------------
     path('dataproducto/',views.listarproductos,name='listarproductos'),
@@ -18,10 +17,8 @@ urlpatterns = [
     path('registrarproducto',views.registrarproductos,name='registrarproducto'),
     path('productos', views.productos, name='productos'),
     path('editarproducto/<str:idProducto>/', views.editarproductos, name='editarproducto'),
-=======
     path('dtventas/', views.listadoventa, name='dtventas'),
     path('registrarventa/', views.registrarventa, name='registrarventa'),
     path('dtventas/eliminarventa/<idVenta>', views.eliminarventa),
     path('dtventas/editarventa/<int:id_venta>', views.editar_venta, name='editar_venta'),
->>>>>>> 9926fc613d0dfa3f5af3b6be0c6a65a33868d1e1
 ]
